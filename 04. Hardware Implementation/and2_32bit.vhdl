fa91e02168ba39c99baaa88dfd91370dbb6641bc
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2024-12-29 01:00:45 +0530

    and2_32bit version 1.0

--- 04. Hardware Implementation/and2_32bit.vhdl
+++ 04. Hardware Implementation/and2_32bit.vhdl
@@ -1,6 +1,6 @@
 -- Create by BG
 -- Created on Sun, 29 Dec 2024
--- Last modified on Sun, 29 Dec 2024
+-- Last modified on Sun, 29 Dec 2024 at 01:00 AM
 -- This is the module for 32 bit AND unit
 
 
