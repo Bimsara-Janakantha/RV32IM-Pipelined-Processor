15d5f96ed4e396ff3f41957f93cd919fa0c84954
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2024-12-30 23:18:38 +0530

    Update MUX2_1.vhdl

--- 04. Hardware Implementation/MUX2_1.vhdl
+++ 04. Hardware Implementation/MUX2_1.vhdl
@@ -20,10 +20,10 @@ use ieee.std_logic_1164.all;
 -- Entity (module)
 entity mux2_1 is
     port(
-        input_1   : in std_logic_vector (31 downto 0);
-        input_2   : in std_logic_vector (31 downto 0);
-        selector  : in std_logic;
-        output_1  : out std_logic_vector (31 downto 0) -- No ; here
+        input_1  : in std_logic_vector (31 downto 0);
+        input_2  : in std_logic_vector (31 downto 0);
+        selector : in std_logic;
+        output_1 : out std_logic_vector (31 downto 0) -- No ; here
     );
 end mux2_1;
 
